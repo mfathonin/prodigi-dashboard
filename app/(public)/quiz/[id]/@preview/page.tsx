cd239405adb30f18280b89411000576970617bb9
Fathoni <mfathonin@gmail.com>
2024-12-29 11:18:31 +0700

    fix: Remove unused async keyword

--- app/(public)/quiz/[id]/@preview/page.tsx
+++ app/(public)/quiz/[id]/@preview/page.tsx
@@ -1,6 +1,6 @@
 import { DownloadButton } from "@/app/(public)/links/[[...paths]]/components/download-button";
 
-export default async function PublicQuizPage() {
+export default function PublicQuizPage() {
   return (
     <div className="[&>p]:text-sm [&>p]:dark:text-zinc-500 [&>p]:text-zinc-700 mt-20">
       <h1 className="text-lg font-semibold mb-4">Akses Terbatas ✋🏻</h1>
