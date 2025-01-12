7f769df4fecfe98497d9491fc668df2541394f71
Fathoni <mfathonin@gmail.com>
2025-01-12 08:03:52 +0700

    chore(quiz): Update `.wellknown/assetlinks.json`

--- app/(public)/.well-known/assetlinks.json/route.ts
+++ app/(public)/.well-known/assetlinks.json/route.ts
@@ -23,7 +23,7 @@ export const GET = () => {
           namespace: "android_app",
           package_name: "com.merahputihperkasa.prodigi",
           sha256_cert_fingerprints: [
-            "19:C9:12:B2:2A:1C:72:A8:12:37:96:7D:F1:CB:98:C8:11:41:2B:4A:FB:96:13:29:74:A2:C0:DC:12:43:54:A4",
+            "14:6B:FB:2A:46:7A:96:93:6A:95:5C:3E:57:A3:6F:63:23:80:DF:A1:89:07:F5:C7:78:D6:93:F5:23:33:E4:0B",
           ],
         },
       },
